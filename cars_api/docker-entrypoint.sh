f84ced406a1a86b96154e2d43d346651349b546a
Aimene AHDIBI <ahdibiaymen@gmail.com>
2024-03-23 14:12:40 -0400

    [Fix] docker entrypoint

--- cars_api/docker-entrypoint.sh
+++ cars_api/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 if [ -n "${MODE}" ] && [ "${MODE}" != "dev" ]; then
-  uvicorn app.main:app --reload
+  uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 else
   uvicorn app.main:app --host 0.0.0.0 --port 8000
 fi
